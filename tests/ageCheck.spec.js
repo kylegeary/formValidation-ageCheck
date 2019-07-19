bdb4cd4e3545181b375cd3770e523a4c9ca44c72
Kyle Geary <Kgeary09@gmail.com>
2019-07-19 13:45:36 -0500

    Create initial tests

---
+++ tests/ageCheck.spec.js
@@ -0,0 +1,47 @@
+import ageCheck from '../src/ageCheck';
+
+describe('Form Validation: ageChecker', () => {
+    describe('settings', () => {
+        it ('should have default message', () => {
+            const expectedVal = 'You must be of age to proceed.';
+            const reqInst = ageCheck();
+            const settings = reqInst.settings;
+
+            expect(settings).toBeDefined();
+            expect(settings).toHaveProperty('message');
+            expect(settings.message).toEqual(expectedVal);
+		});
+
+		it('should have default messageAttr', () => {
+            const expectedVal = 'data-validation-agecheck-message';
+            const ageCheckInst = ageCheck();
+            const settings = ageCheckInst.settings;
+
+            expect(settings).toBeDefined();
+            expect(settings).toHaveProperty('messageAttr');
+            expect(settings.messageAttr).toEqual(expectedVal);
+		});
+
+		it('should have default events', () => {
+            const ageCheckInst = ageCheck();
+            const settings = ageCheckInst.settings;
+
+            expect(settings).toBeDefined();
+            expect(settings).toHaveProperty('events');
+            expect(settings.events).toHaveLength(2);
+            expect(settings.events).toContain('focusout');
+            expect(settings.events).toContain('submit');
+        });
+
+		it('should have default minAge', () => {
+			const expectedVal = 0;
+            const ageCheckInst = ageCheck();
+            const settings = ageCheckInst.settings;
+
+            expect(settings).toBeDefined();
+            expect(settings).toHaveProperty('minAge');
+            expect(settings.minAge).toEqual(expectedVal);
+		});
+
+	});
+})
